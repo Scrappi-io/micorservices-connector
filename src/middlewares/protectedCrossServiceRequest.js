972b854288bf4dfeb282103d897c447fb9832b0a
Alexey <akhachatryan@bytechek.com>
2022-09-29 16:27:12 +0400

    Localhost or 127.0.0.1 as request host

--- src/middlewares/protectedCrossServiceRequest.js
+++ src/middlewares/protectedCrossServiceRequest.js
@@ -1,6 +1,9 @@
 module.exports = (config) => {
   return (req, res, next) => {
-    if (req.headers['cross-service-token'] !== config.crossServiceToken || req.hostname !== 'localhost') {
+    if (
+      req.headers['cross-service-token'] !== config.crossServiceToken ||
+      (req.hostname !== 'localhost' && req.hostname !== '127.0.0.1')
+    ) {
       throw new Error('403 Forbidden');
     }
 
